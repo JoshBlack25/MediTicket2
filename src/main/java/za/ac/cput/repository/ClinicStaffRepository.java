8688670a60987b1b666300a5e5f01d617bcc3a6f
Marlon Bam <230123456@mycput.ac.za>
2026-07-05 16:36:34 +0200

    Add ClinicStaffRepository

--- src/main/java/za/ac/cput/repository/ClinicStaffRepository.java
+++ src/main/java/za/ac/cput/repository/ClinicStaffRepository.java
@@ -1,4 +1,12 @@
 package za.ac.cput.repository;
 
-public class ClinicStaffRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.user.ClinicStaff;
+
+import java.util.Optional;
+
+public interface ClinicStaffRepository extends JpaRepository<ClinicStaff, Integer> {
+
+    Optional<ClinicStaff> findByEmail(String email);
+
 }
